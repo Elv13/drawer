78560df05533ebe84f754feb1b4bb8acb3861135
Axxx <axxx@wilson>
2014-10-18 01:43:24 +0200

    Semicolon separated data file

--- topCpu3.sh
+++ topCpu3.sh
@@ -20,7 +20,7 @@ function testSlash {
     fi
 }
 
-echo "cpuStat = {"
+
 for LINE in `/bin/ps -e -o pid,pcpu,args --sort -pcpu | grep -v "0.0"`;do
     PID=`echo $LINE | awk '{print $1}'`
     PERCENT=`echo $LINE | awk '{print $2}'`
@@ -33,9 +33,7 @@ for LINE in `/bin/ps -e -o pid,pcpu,args --sort -pcpu | grep -v "0.0"`;do
         COMMAND=${COMMAND:1}
     done
     if [ "$PERCENT" != "%CPU" ]; then
-       echo "{  pid = $PID, percent = \"$PERCENT\", args = \"$COMMAND\", name = \"`echo $COMMAND | cut -f1 -d' '`\" },"
+       echo "$PID;$PERCENT;$COMMAND;`echo $COMMAND | cut -f1 -d' '`"
     fi
     SKIP="false"
 done
-echo "{  pid = 0, percent = \"0\", args = \"0\", name = \"0\" }"
-echo }
