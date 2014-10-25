67a108478c260fdbc60778ebd663155994ec11db
Axxx <axxx@wilson>
2014-10-25 17:22:43 +0200

    New cpuInfo script

---
+++ cpuInfo3.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+#Set localization for bc localizaton
+LC_NUMERIC="en_US.UTF-8"
+
+CPU_INFO=`cat /proc/cpuinfo`
+SENSOR=`sensors`
+CPU_MODEL=`echo -e "$CPU_INFO" | grep "model name" | grep -e ":[0-9a-zA-Z()@., ]*" -o | head -n 1`
+CPU_MODEL=${CPU_MODEL:2}
+CORE_NB=`echo -e "$CPU_INFO" | grep processor | tail -n 1 | awk '{print $3}'`
+
+
+
+#COUNTER=0
+#CPU_STATE=`mpstat -P ALL 1 1 | head -n 8 | tail -n 4`
+IFS_BACK=$IFS
+IFS=`echo -en "\n\b"`
+CORE_INFO=0
+for CORE in `seq 0 $CORE_NB`; do
+#echo "Core: $CORE"
+	#Add usr, nice and sys
+	#USAGE=`echo $CORE | awk ' {print $3 "+" $4 }' | bc`
+    #echo "line $CORE: $CPU_INFO | grep 'cpu MHz' | cut -d':' -f 2 | sed -n $COREp"
+    USAGE=$(cat /proc/cpuinfo | grep "cpu MHz" | cut -d":" -f 2 | sed -n $(printf "%sp" $(($CORE+1))))
+    #echo "Usage: $USAGE"
+    USAGE=`printf "%.0f" $USAGE`
+    IOWAIT=`echo $CORE | awk ' {print $6}'`
+	IOWAIT=`printf "%.0f" $IOWAIT`
+	IDLE=`echo $CORE | awk ' {print $11}'`
+	IDLE=`printf "%.0f" $IDLE`
+	#echo "  <b>use=</b><i>$USAGE%</i>, <b>I/O=</b><i>$IOWAIT%</i>, <b>idle=</b><i>$IDLE%</i>" > /tmp/cpuStat.${COUNTER}
+	CORE_INFO[$CORE]="usage =\"$USAGE\", iowait = \"$IOWAIT\", idle = \"$IDLE\""
+    
+	let COUNTER=$COUNTER+1
+done
+
+IFS=$IFS_BACK
+echo "cpuInfo = {"
+echo "   model = \"$CPU_MODEL\","
+#echo "   core = $CORE_NB"
+echo "   overallLoad = `top -bn1 | grep 'Cpu(s)' | sed 's/.*, *\([0-9.]*\)%* id.*/\1/' | bc`,"
+for CURRENT_CORE in `seq 0 $CORE_NB`;do
+    CPU_CORE=`echo -e "$CPU_INFO" | grep -e "processor[	]*: $CURRENT_CORE" --context +6 | grep "cpu MHz"`
+
+    CORE_ONE=`echo -e "$CPU_CORE" | head -n 1 | grep -e "[0-9.]*" -o`
+    CORE_ONE=`printf "%.0f" $CORE_ONE`
+    TEMP_ONE=`echo -e "$SENSOR" | grep "Core $CURRENT_CORE" | grep -e "   +[0-9]*" -o`
+    echo "   core${CURRENT_CORE} = { speed= \"${CORE_ONE}\", temp= \"${TEMP_ONE:4}C\", ${CORE_INFO[${CURRENT_CORE}]}},"
+done
+echo "}"
