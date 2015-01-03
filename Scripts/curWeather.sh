35f6aa824d2b45988395081805a36db6f5b0de01
AxelTB <axel99999@hotmail.com>
2015-01-03 15:32:09 +0100

    CCleaning old scripts

--- Scripts/curWeather.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl -s "http://api.wunderground.com/auto/wui/geo/ForecastXML/index.xml?query=${@:-<YOURZIPORLOCATION>}"|perl -ne '/<title>([^<]+)/&&printf "%s: ",$1;/<fcttext>([^<]+)/&&print $1,"\n"' | head -n 1 | cut -d: -f2 
